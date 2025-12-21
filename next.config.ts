34be47df8b9df8a6fdce077613191ff706a60a62
TechKing <obafemielijahsunday@gmail.com>
2025-12-21 09:59:37 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/meeting",
+        permanent: false, // Changed from true to false
+      },
+    ];
+  },
 };
 
 export default nextConfig;
